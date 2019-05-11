d8ce289a2a713a4eda9b44d9b807e28df77128a4
VictoriaSs <p.viktoria@mail.ru>
2019-05-11 21:07:08 +0400

    Таблица умножения

---
+++ src/Task03.java
@@ -0,0 +1,9 @@
+public class Task03 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // Woundefull world!
+        /*
+        You can write everything you want
+         */
+    }
+}
