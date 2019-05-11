bf0363a3bf34a2b0d29654ab65a83d82410ca694
VictoriaSs <47081487+VictoriaSs@users.noreply.github.com>
2019-05-11 20:22:44 +0300

    Delete Task03.java

--- src/Task03.java
+++
@@ -1,9 +0,0 @@
-public class Task03 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        // Woundefull world!
-        /*
-        You can write everything you want
-         */
-    }
-}
