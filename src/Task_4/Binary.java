2be2c9deb0a5df116e43498fa5f5df09de4bae66
VictoriaSs <p.viktoria@mail.ru>
2019-05-12 09:04:46 +0400

    Отработка поразрядных операций

---
+++ src/Task_4/Binary.java
@@ -0,0 +1,12 @@
+package Task_4;
+
+public class Binary{
+        public static void main (String[] args){
+        byte b = (byte) 192;
+        System.out.println(Integer.toBinaryString((int)b));
+        System.out.println((int)b);
+        }
+}
+
+
+
